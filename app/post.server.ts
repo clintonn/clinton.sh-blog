1febd260471dd267ae32a384e96a281471deb9e7
Clinton Nguyen <clinton.ngyn@gmail.com>
2022-11-21 19:40:44 -0800

    madness

--- app/post.server.ts
+++ app/post.server.ts
@@ -3,13 +3,23 @@ import fs from "fs";
 import { unified } from "unified";
 import remarkParse from "remark-parse";
 import remarkFrontmatter from "remark-frontmatter";
+import remarkParseFrontmatter from "remark-parse-frontmatter";
+import remarkRehype from "remark-rehype";
+import compiler from "rehype-stringify";
+import { parse as yamlParse } from "yaml";
 import { json } from "@remix-run/node";
 
 export const getPost = async (slug: string) => {
   const postsDirectory = path.resolve(__dirname, "../posts");
   try {
     const post = fs.readFileSync(`${postsDirectory}/${slug}.md`, "utf8");
-    const file = unified().use(remarkParse).use(remarkFrontmatter).parse(post);
+    const file = await unified()
+      .use(remarkParse)
+      .use(compiler)
+      .use(remarkFrontmatter)
+      .use(remarkParseFrontmatter, { yaml: yamlParse })
+      .use(remarkRehype, { allowDangerousHtml: true })
+      .process(post);
     console.log(file);
     return json(file);
   } catch (error) {

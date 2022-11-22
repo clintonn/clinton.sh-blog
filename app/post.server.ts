9c05ed68ae66583ff04d499399fb912d6c06ec4f
Clinton Nguyen <clinton.ngyn@gmail.com>
2022-11-21 19:02:30 -0800

    wip...

--- app/post.server.ts
+++ app/post.server.ts
@@ -1,12 +1,19 @@
 import path from "path";
 import fs from "fs";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkFrontmatter from "remark-frontmatter";
+import { json } from "@remix-run/node";
 
-export const getPost = (slug: string) => {
+export const getPost = async (slug: string) => {
   const postsDirectory = path.resolve(__dirname, "../posts");
   try {
     const post = fs.readFileSync(`${postsDirectory}/${slug}.md`, "utf8");
-    return post;
-  } catch {
+    const file = unified().use(remarkParse).use(remarkFrontmatter).parse(post);
+    console.log(file);
+    return json(file);
+  } catch (error) {
+    console.log(error);
     return null;
   }
 };
